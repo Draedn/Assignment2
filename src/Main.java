4caf40b8915c967e8d0ae6e29ad35759fcbe07d7
Draedn <draedn.groves@dcmail.ca>
2024-02-15 21:09:30 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+/*
+Title: Dart Game Program
+Author: Draedn Groves
+Date: Feb.13th/2024
+Purpose: Create a Dart Game
+ */
+
+import java.util.Scanner;
+
+// Class container for everything
+public class Main {
+
+    // Main method - The basics of how the program will run
+    public static void main(String[] args) {
+        startProgram();
+        runProgram();
+        endProgram();
+    }
+
+    // <editor-fold desc="Start Method">
+
+    public static void startProgram(){
+
+    }
+    // </editor-fold>githu
+
+    // <editor-fold desc="Run Method">
+    public static void runProgram(){
+
+    }
+    // </editor-fold>
+
+    // <editor-fold desc="End Method">
+    public static void endProgram(){
+
+    }
+    // </editor-fold>
+}
---
+++ src/Tools.java
@@ -0,0 +1,12 @@
+/*
+Title: Tools
+Author: Draedn Groves
+Date: Feb.13th/2024
+Purpose: Tools
+ */
+
+
+
+public class Tools {
+
+}
